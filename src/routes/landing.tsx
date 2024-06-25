f555dccee498ee2ab7fee148841137650d22f76f
Aryan Trivedi <74698706+AryanTrivedi865@users.noreply.github.com>
2024-06-25 12:53:24 +0530

    main

---
+++ src/routes/landing.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./../styles/landing.css";
+
+function Landing() {
+  return (
+    <div className="landing">
+      <header className="landing-header">
+        <h1>Welcome to React</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Landing;
