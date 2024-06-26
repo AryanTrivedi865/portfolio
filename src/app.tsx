8a9fd92b3dc65c8089ab081f9ca71c86b054e0b2
Aryan Trivedi <74698706+AryanTrivedi865@users.noreply.github.com>
2024-06-26 15:14:16 +0530

    main

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
-import About from "./screen/navbar/about";
-import Home from "./screen/navbar/home";
-import Contacts from "./screen/navbar/contacts";
-import Signup from "./screen/navbar/signup";
+import About from "./screen/routes/about";
+import Home from "./screen/routes/home";
+import Contacts from "./screen/routes/contacts";
+import Signup from "./screen/routes/signup";
 import Navbar from "./components/navbar";
 import NotFoundPage from "./screen/page404";
 import Landing from "./screen/landing";
+import "./styles/app.css"
 
 function Layout() {
   const location = useLocation();
@@ -14,19 +15,18 @@ function Layout() {
   const navbarPath = ["/home", "/", "/about", "/contacts", "/signup"];
 
   return (
-    <div style={{
-      backgroundColor: "red",
-      height: "100vh",
-      color: "white",
-    }}>
-      {navbarPath.includes(location.pathname) && <Navbar />}
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contacts" element={<Contacts />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="*" element={<NotFoundPage />} />
+    <div className="parent">
+      {
+      navbarPath.includes(location.pathname) && 
+        <Navbar/>
+      }
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>
   );
