0afde2bd689ab05c8c9e4f8d52d66042af887786
Aryan Trivedi <74698706+AryanTrivedi865@users.noreply.github.com>
2024-06-27 14:11:44 +0530

    chore: Add preconnect links for Google Fonts and update font-family in app.css

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,15 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import "./styles/app.css"
-import PageNotFound from "./screen/routes/page-not-found";
 
 function Layout() {
+
   const location = useLocation();
 
   const navbarPath = ["/home", "/", "/about", "/contacts", "/signup"];
 
   return (
     <div className="parent">
-        <Routes>
-          <Route path="/" element={
-            <h1>WELCOME TO WEBSITE</h1>
-          } />
-          <Route path="*" element={<PageNotFound/>} />
-      </Routes>
+        <Routes/>
     </div>
   );
 }
