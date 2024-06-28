1bb3dc37b876e8f757308d51abe93a68b751fc10
Aryan Trivedi <74698706+AryanTrivedi865@users.noreply.github.com>
2024-06-28 22:03:27 +0530

    chore: Update reportWebVitals import path and remove unused setupTests file

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './app';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './utils/react-default/report-web-vitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
