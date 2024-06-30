9203b6a887b9526decf66001209180a9a6587440
Aryan Trivedi <74698706+AryanTrivedi865@users.noreply.github.com>
2024-06-30 11:18:29 +0530

    chore: Update navbar component and styles

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,15 +1,17 @@
 import { Link } from "react-router-dom";
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./../styles/navbar.css";
+import Hamburger from "hamburger-react";
+import { useState } from "react";
 
 function Navbar() {
+    const [isOpen, setOpen] = useState();
 
     const socialLinks = [
         { name: "Github", url: "https://www.github.com/aryantrivedi865" },
         { name: "Twitter", url: "https://www.twitter.com" },
         { name: "LinkedIn", url: "https://www.linkedin.com/in/aryan-trivedi-251679279/" },
         { name: "Instagram", url: "https://www.instagram.com/aryan_.__" },
-
     ];
 
     return (
@@ -29,15 +31,28 @@ function Navbar() {
                 <div className="navbar__contact__social">
                     <ul>
                         {socialLinks.map((link, index) => (
-                                <li key={index}>
-                                    <a href={link.url} target="_blank" rel="noopener noreferrer">
-                                        <i className={`fab fa-${link.name.toLowerCase()}`}></i>
-                                        {link.name}
-                                    </a>
-                                </li>
-                                )
-                            )
-                        }
+                            <li key={index}>
+                                <a href={link.url} target="_blank" rel="noopener noreferrer">
+                                    <i className={`fab fa-${link.name.toLowerCase()}`}></i>
+                                    {link.name}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+            <div className="navbar__hamburger">
+                <Hamburger toggled={isOpen} toggle={setOpen} size={20} />
+                <div className="navbar__hamburger__menu" style={{ display: isOpen ? 'block' : 'none' }}>
+                    <ul>
+                        {socialLinks.map((link, index) => (
+                            <li key={index}>
+                                <a href={link.url} target="_blank" rel="noopener noreferrer">
+                                    <i className={`fab fa-${link.name.toLowerCase()}`}></i>
+                                    {link.name}
+                                </a>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </div>
